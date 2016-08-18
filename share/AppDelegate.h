1c90dd09cae856a52f30ff7a1328e1bdb5e79ddb
hh <972958298@qq.com>
2016-08-18 09:51:27 +0800

    Initial Commit

---
+++ share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  share
+//
+//  Created by dongkaiqiang on 16/8/18.
+//  Copyright © 2016年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
